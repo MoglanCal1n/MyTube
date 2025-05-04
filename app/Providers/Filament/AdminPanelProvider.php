bb2c8bcb46528e2080f2f0f6f067db3d1ada06ed
Moglan Calin <moglancalin5@gmail.com>
2025-05-04 11:43:18 +0300

    changed the app path from admin to mytube

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,8 +24,8 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('mytube')
+            ->path('mytube')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
